6cef9431694624cb58f76fad480c676e96c47aee
小强何 <1565706716@qq.com>
2018-01-22 17:19:02 +0800

    hwq-

---
+++ app/src/main/java/com/xiaoqianghe/koltin/basekoltin/ui/activity/AboutActivity.kt
@@ -0,0 +1,13 @@
+package com.xiaoqianghe.koltin.basekoltin.ui.activity
+
+
+/**
+ *
+ * Author：Wq
+ * Date：2018/1/22 16:51
+ * Description：//todo
+ *
+ *
+ */
+class AboutActivity {
+}
---
+++ app/src/main/java/com/xiaoqianghe/koltin/basekoltin/ui/activity/ProfileHomePageActivity.kt
@@ -0,0 +1,13 @@
+package com.xiaoqianghe.koltin.basekoltin.ui.activity
+
+
+/**
+ *
+ * Author：Wq
+ * Date：2018/1/22 16:38
+ * Description：//todo
+ *
+ *
+ */
+class ProfileHomePageActivity {
+}
